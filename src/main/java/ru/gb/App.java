a1d95908dbe71e66de233feb672d976a5a7a3c7d
danfilof <danila.anfilofyev@gmail.com>
2022-01-15 17:28:59 +0100

    First task completed

--- src/main/java/ru/gb/App.java
+++ src/main/java/ru/gb/App.java
@@ -1,4 +1,24 @@
 package ru.gb;
 
+import java.util.*;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        //FIRST TASK
+        // инициализирую listArray
+        List<String> artists = Arrays.asList("The Beatles", "Elvis Presley", "The Beatles", "The Rolling Stones", "Chuck Berry", "Bob Marley", "Queen", "The Beatles", "Chuck Berry", "Queen",
+                "Led Zeppelin", "Queen", "The Beatles", "AC/DC", "Nirvana", "Johnny Cash", "Michael Jackson", "AC/DC", "Nirvana", "Queen" );
+        // инициализирую Hashset, так как он убирает повторяющиеся значения ячеек
+        Set<String> uniqueArtists = new HashSet<String>(artists);
+
+        System.out.println("Уникальные артисты: " + uniqueArtists.toString());
+
+        // Запускаю цикл по коллекции, где ключи - уникальные исполнители. На сайте stackoverflow нашел информацию про функцию Collections.frequency, использую ее для упрощения работы
+        for (String key : uniqueArtists) {
+            System.out.println(key + " встречались в коллекции " + Collections.frequency(artists, key) + " раз.");
+        }
+
+    }
 }
